1653b4378f545212173bce10fb20e49832fda5e6
Ashe <molly.02.andrews@gmail.com>
2023-10-25 14:36:33 -0400

    File cleanup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <body>
+      <div className="button-group">
+        <button className="config-button">4x4</button>
+        <button className='config-button'>5x5</button>
+        <button classname='config-button'>6x6</button>
+      </div>
+
+      <div className='game-board'>
+        <p>Game Board goes here</p>
+      </div>
+
+      <div className='button-group'>
+        <button className='game-button'>Reset Game</button>
+        <button className='game-button'>Complete Game</button>
+      </div>
+      <div className="button-group">
+        <button className="move-button">Up</button>
+        <button className='move-button'>Down</button>
+        <button classname='move-button'>Left</button>
+        <button classname='move-button'>Right</button>
+      </div>
+    </body>
   );
 }
 
